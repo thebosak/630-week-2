9fe12598c6ceea9526e3eb7ae571936de0b0aa3b
thebosak <33043855+thebosak@users.noreply.github.com>
2019-05-14 21:51:36 -0500

    initial commit

---
+++ main/Main.py
@@ -0,0 +1,11 @@
+'''
+Created on May 14, 2019
+
+@author: Brad Bosak
+'''
+
+import Rectangle
+
+rect_1=Rectangle(5,2)
+
+print(rect_1.area())
---
+++ rectangle/Rectangle.py
@@ -0,0 +1,33 @@
+'''
+Created on May 13, 2019
+
+@author: Brad Bosak
+'''
+
+class Rectangle:
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, length, width):
+        self.length=length
+        self.width=width
+        
+        
+    def area(self):
+        area=self.length*self.width
+        
+        return area
+
+def getNumbers():
+    length = eval(input("Enter the length: "))
+    width  = eval(input("Enter the width: "))
+    return length, width
+
+def main():
+    length,width = getNumbers()
+    rect=Rectangle(length,width)
+    print(rect.area())
+    
+main()
